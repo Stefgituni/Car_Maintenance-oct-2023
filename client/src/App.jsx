5ce59ee50712bf297dbc98783f977651f966639e
Stefgituni <122815876+Stefgituni@users.noreply.github.com>
2023-11-17 18:42:28 +0200

    Add initial react project

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-no-target-blank */
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <h1>Car Maintenance</h1>
+    </>
+  )
+}
+
+export default App
